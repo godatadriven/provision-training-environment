6186045b964764bf655e874ebcea3ed4c2259d43
Alexander Bij <AlexanderBij@godatadriven.com>
2018-11-16 15:36:15 +0100

    Use the latest version of conda

--- roles/bootstrap/templates/install_conda.sh
+++ roles/bootstrap/templates/install_conda.sh
@@ -1,6 +1,7 @@
 if [ ! -d "/anaconda" ]; then
-    wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
-    bash Anaconda3-4.2.0-Linux-x86_64.sh -b -p /anaconda
+    # wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+    wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
+    bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /anaconda
     echo "export PATH=/anaconda/bin:$PATH" >> /etc/bash.bashrc
     echo "export PYSPARK_PYTHON=/anaconda/bin/python" >> /etc/bash.bashrc
 fi
